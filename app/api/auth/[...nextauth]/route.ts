44bb06471db8f28c73321ca8bd44476e3541206e
ashir <ashirhashmi26@gmail.com>
2024-10-29 14:52:47 -0400

    fix: issue while uploading to vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,62 +3,56 @@ import GoogleProvider from "next-auth/providers/google";
 import { ConnectToDB } from "@utils/database";
 import User from "@models/user";
 
-interface profileProps {
-    profile: any
-}
 interface sessionProps {
-    session: any
+  session: any;
 }
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        })
-    ],
-    callbacks: {
-        async session({ session }: sessionProps) {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async session({ session }: sessionProps) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
 
-            const sessionUser = await User.findOne({
-                email: session.user.email
-            })
+      session.user.id = sessionUser._id.toString();
 
-            session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }: any) {
+      try {
+        await ConnectToDB();
 
-            return session;
+        const userExists = await User.findOne({
+          email: profile.email,
+        }).catch((err) => {
+          console.log("Error in finding user: ", err.message);
+          throw new Error("Database query failed");
+        });
 
-        },
-        async signIn({ profile }: profileProps) {
-            try {
-                await ConnectToDB();
-
-                const userExists = await User.findOne({
-                    email: profile.email
-                }).catch(err => {
-                    console.log("Error in finding user: ", err.message);
-                    throw new Error("Database query failed");
-                });
-
-                if (!userExists) {
-                    await User.create({
-                        email: profile.email,
-                        username: profile.name.replace(" ", "").toLowerCase(),
-                        image: profile.picture
-                    }).catch(err => {
-                        console.log("Error creating new user: ", err.message);
-                        throw new Error("User creation failed");
-                    });
-                }
-
-
-                return true;
-            } catch (error: any) {
-                console.log("Error checking if user exists: ", error.message);
-                return false;
-            }
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          }).catch((err) => {
+            console.log("Error creating new user: ", err.message);
+            throw new Error("User creation failed");
+          });
         }
-    }
-})
 
-export { handler as GET, handler as POST }
+        return true;
+      } catch (error: any) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
