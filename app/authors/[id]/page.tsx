8d0e88104e11e0136491eb36f78a153660d99a7c
ashirhash <ashirhashmi26@gmail.com>
2025-01-28 17:27:35 -0500

    improve: made authors a server component

--- app/authors/[id]/page.tsx
+++ app/authors/[id]/page.tsx
@@ -1,55 +1,33 @@
-"use client";
-import { useState, useEffect } from "react";
 import Profile from "@components/Profile";
 
-interface Post {}
-
-interface Creator {
-  username: string;
-}
-
-interface AuthorProfileProps {
+type AuthorProps = {
   params: {
     id: string;
   };
-}
+};
 
-const AuthorProfile = ({ params }: AuthorProfileProps) => {
-  const [loading, setLoading] = useState(true);
-  const [creator, setCreator] = useState<Creator | null>(null);
-  const [posts, setPosts] = useState<Post[]>([]);
+const fetchPosts = async (id: string) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/users/${id}/posts`
+    );
+    if (!response.ok) throw new Error("Network response was not ok");
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to fetch user data:", error);
+  }
+};
 
+export default async function AuthorProfile({ params }: AuthorProps) {
   const { id } = params;
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch(`/api/users/${id}/posts`);
-        if (!response.ok) throw new Error("Network response was not ok");
-
-        const posts = await response.json();
-        setCreator(posts[0]?.creator || null);
-        setPosts(posts || []);
-
-      } catch (error) {
-        console.error("Failed to fetch user data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (id) fetchUserData();
-  }, [id]);
-
-  if (loading) return <div className="desc">Loading...</div>;
+  const posts = await fetchPosts(id);
+  console.log(posts);
 
   return (
     <Profile
-      name={`${creator?.username || "User"}'s`}
+      name={`${posts[0]?.creator?.username || "User"}'s`}
       desc="Welcome to my profile"
       posts={posts}
     />
   );
-};
-
-export default AuthorProfile;
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,15 +5,6 @@ import { ConnectToDB } from "@utils/database";
 
 export default async function Page() {
   const session: any = await getServerSession(authOptions);
-  
-  async function fetchPosts() {
-    await ConnectToDB();
-    const response = await fetch(`${process.env.NEXTAUTH_URL}/api/users/${session?.user.id}/posts`);
-    return await response.json();
-  }
-
-  const posts = await fetchPosts();
-
   if (!session) {
     return (
       <div>
@@ -21,6 +12,16 @@ export default async function Page() {
       </div>
     );
   }
+
+  async function fetchPosts() {
+    await ConnectToDB();
+    const response = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/users/${session?.user.id}/posts`
+    );
+    return await response.json();
+  }
+
+  const posts = await fetchPosts();
 
   return (
     <>
